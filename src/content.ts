dda3b8fe15b1c7776a646b65f01d3f5439e80441
Nicola Toscan <30284178+nicolatoscan@users.noreply.github.com>
2023-06-30 09:17:41 +0200

    Fix regex map

--- src/content.ts
+++ src/content.ts
@@ -17,14 +17,14 @@ async function copyToTheClipboard(t: string) {
     t = t.slice(1)
   if (t.endsWith('"'))
     t = t.slice(0, -1)
-  t = t.replace(/\\n/g, '\n').replace(/\${(\w+)}/g, '@$1');
+  t = t.replace(/\\n/g, '\n').replace(/\{\$(\w+)}/g, '@$1');
 
   await navigator.clipboard.writeText(t);
 }
 
 async function pasteFromClipboard() {
   let t = await navigator.clipboard.readText();
-  t = t.replace(/\r\n/g, '\\n').replace(/\n/g, '\\n').replace(/@(\w+)/g, '${$1}')
+  t = t.replace(/\r\n/g, '\\n').replace(/\n/g, '\\n').replace(/@(\w+)/g, '{$$1}')
   if (!t.startsWith('"'))
     t = `"${t}`;
   if (!t.endsWith('"'))
